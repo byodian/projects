fd500300f593dc8323db6f586dac19540bf42190
byodian <byj6696@gmail.com>
2020-12-24 22:39:18 +0800

    Event loop & Event handler

--- js-example/src/example.js
+++ js-example/src/example.js
@@ -7,21 +7,87 @@ var app = {
     li.textContent = message;
     elem.appendChild(li);
   },
-  elements: {
-    second: '.second'
+  report: function(message) {
+    const results = document.querySelector('.results');
+    if (!results) return;
+
+    const li = document.createElement('li');
+    li.className = message ? 'report' : 'fail';
+    li.appendChild(document.createTextNode(message));
+    results.appendChild(li);
+  },
+  assert: function (value, desc) {
+    var li = document.createElement('li');
+    li.className = value ? 'pass' : 'fail';
+    li.appendChild(document.createTextNode(desc));
+    document.querySelector('.results').appendChild(li);
+  },
+  firstHandler: function(event) {
+    const results = document.querySelector('.results');
+    if (!results) return;
+
+    while(results.lastChild) {
+      results.removeChild(results.lastChild);
+    }
+  },
+  secondHandler: function(event) {
+    console.log(event.type);
+  },
+  generateTable: function() {
+    const rowCount = 20000;
+    const divideInto = 4;
+    const chunkSize = rowCount / divideInto; // 5000
+    let iteration = 0;
+    const table = document.getElementsByTagName('tbody')[0];
+    setTimeout(function generateRows() {
+      const base = chunkSize * iteration;
+      for (let i = 0; i < chunkSize; i++) {
+        const tr = document.createElement('li');
+        for (let t = 0; t < 6; t++) {
+          const td = document.createElement('td');
+          td.appendChild(document.createTextNode((i + base) + ',' + t + ',' + iteration));
+          tr.appendChild(td);
+        }
+        table.appendChild(tr);
+      }
+      console.log(iteration);
+
+      iteration++;
+      if (iteration < divideInto) {
+        setTimeout(generateRows, 0);
+      }
+
+    }, 0);
+  },
+  elems: {
+    second: '.second',
+    firstButton: '#firstButton',
+    secondButton: '#secondButton',
+    outerContainer: '#outerContainer',
+    innerContainer: '#innerContainer',
+    table: 'table',
+    tbody: 'tbody'
   }
 }
 
-document.body.addEventListener('mousemove', function() {
-  app.addMessage(
-    app.elements.second,
-    'Event: mousemove'
-  )
-})
+document
+  .querySelector(app.elems.firstButton)
+  .addEventListener('click', app.firstHandler)
+document
+  .querySelector(app.elems.secondButton)
+  .addEventListener('click', app.secondHandler)
 
-document.body.addEventListener('click', function() {
-  app.addMessage(
-    app.elements.second,
-    'Event: Click'
-  )
+const outerContainer = document.querySelector(app.elems.outerContainer);
+const innerContainer = document.querySelector(app.elems.innerContainer);
+
+outerContainer.addEventListener('click', function(event) {
+  app.report('OuterContainer handler: capture');
+  app.assert(this === outerContainer, "This referes to the outerContainer");
+  app.assert(event.target === innerContainer, 'event.target referes to the innerContainer');
+}, true)
+
+innerContainer.addEventListener('click', function(event) {
+  app.report('innerContainer handler: bubble');
+  app.assert(this === innerContainer, "This referes to the innerContainer");
+  app.assert(event.target === innerContainer, 'event.target referes to the innerContainer');
 })
