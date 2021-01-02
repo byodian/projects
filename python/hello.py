04bf9ef9ba0b259835d7fefd112a2c887c179542
byodian <byj6696@gmail.com>
2021-01-02 23:53:32 +0800

    Add python

---
+++ python/hello.py
@@ -0,0 +1,8 @@
+print("Hello world!")
+
+# Variables
+a = 28  # int
+b = 1.5  # float
+c = "Hello"  # str
+d = True  # bool
+e = None  # NoneType
---
+++ python/name.py
@@ -0,0 +1,12 @@
+name = input("Name: ")
+print("Hello, " + name)
+print(f"Hello, {name}")
+
+n = input("Number: ")
+
+if n > 0:
+    print("n is positive")
+elif n < 0:
+    print("n is negative")
+else:
+    print("n is not positive")
