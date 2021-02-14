d0092066548c850402610fbe14bbc5bc71619d2a
byodian <byj6696@gmail.com>
2021-02-14 21:49:48 +0800

    Update the backend codes

--- fullstack/part2/phonebook_backend/index.js
+++ fullstack/part2/phonebook_backend/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
 
+const requestLogger = (request, response, next) => {
+  console.log('Method: ', request.method);
+  console.log('Path: ', request.path);
+  console.log('Body: ', request.body);
+  console.log(request);
+  console.log('____');
+  next();
+}
+
 app.use(express.json());
+app.use(requestLogger);
 
 let persons = [
   {
