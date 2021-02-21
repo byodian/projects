72783bb0434ccf7db816e2787e0f8acc8d132489
byoidan <byj6696@gmail.com>
2021-02-21 21:53:10 +0800

    Backend connected to a database

--- fullstack/part2/backend/notes_backend/index.js
+++ fullstack/part2/backend/notes_backend/index.js
@@ -1,6 +1,33 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 
+const url = process.env.MONGODB_URL;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+})
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+})
+
+const Note = mongoose.model('Note', noteSchema);
+
+const app = express();
 const setHeaders = (request, response, next) => {
   response.set('Access-Control-Allow-Origin', 'http://localhost:3000');
   response.set('Access-Control-Allow-Methods', 'PUT,DELETE,GET,POST');
@@ -13,33 +40,14 @@ app.use(express.json());
 app.use(setHeaders);
 app.use(express.static('build'));
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hey There!</h1>');
 });
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes);
+  Note.find({}).then(notes => {
+    response.json(notes);
+  })
 });
 
 app.get('/api/notes/:id', (request, response) => {
---
+++ fullstack/part2/backend/notes_backend/mongodb.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb://yodi:${password}@cluster0-shard-00-00.lvcr8.mongodb.net:27017,cluster0-shard-00-01.lvcr8.mongodb.net:27017,cluster0-shard-00-02.lvcr8.mongodb.net:27017/note-app?ssl=true&replicaSet=atlas-joviuu-shard-0&authSource=admin&retryWrites=true&w=majority`;
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+  console.log("We're connected");
+})
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema);
+console.log(Note);
+
+const note = new Note({
+  content: 'HTML is Easy',
+  date: new Date(),
+  important: true,
+})
+
+// Note.find({}).then(result => {
+//   result.forEach(note => {
+//     console.log(note);
+//   })
+//   db.close();
+// })
+
+
+note.save()
+  .then(result => {
+    console.log('note saved!');
+    mongoose.connection.close();
+  })
+  .catch(error => {
+    console.log(error);
+  })
