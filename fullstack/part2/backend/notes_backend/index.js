2b2657bf36044db8cb3d8418b7d0c27bda8dca6d
byoidan <byj6696@gmail.com>
2021-02-28 15:31:35 +0800

    Adding validation before storing data to the  database

--- fullstack/part2/backend/notes_backend/index.js
+++ fullstack/part2/backend/notes_backend/index.js
@@ -13,9 +13,12 @@ const setHeaders = (request, response, next) => {
 
 const errorHandler = (error, request, response, next) => {
   console.log(error.message);
+  console.log(error.name);
 
   if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id'});
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
@@ -29,10 +32,13 @@ app.get('/', (request, response) => {
   response.send('<h1>Hey There!</h1>');
 });
 
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes);
-  })
+app.get('/api/notes', (request, response, next) => {
+  Note
+    .find({})
+    .then(notes => {
+      response.json(notes);
+    })
+    .catch(error => next(error));
 });
 
 app.get('/api/notes/:id', (request, response, next) => {
@@ -47,7 +53,7 @@ app.get('/api/notes/:id', (request, response, next) => {
   .catch(error => next(error));
 });
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
   
   if (body.content === undefined) {
@@ -62,9 +68,13 @@ app.post('/api/notes', (request, response) => {
     date: new Date(),
   });
   
-  note.save().then(savedNote => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => {
+      response.json(savedAndFormattedNote);
+    })
+    .catch(error => next(error));
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
