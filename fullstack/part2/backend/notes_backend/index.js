d45c3f0346a3d2089f0646f1386cb91f71fda2ff
byoidan <byj6696@gmail.com>
2021-02-28 11:46:15 +0800

    Moving error handling into middleware

--- fullstack/part2/backend/notes_backend/index.js
+++ fullstack/part2/backend/notes_backend/index.js
@@ -11,6 +11,16 @@ const setHeaders = (request, response, next) => {
   next();
 }
 
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id'});
+  }
+
+  next(error);
+}
+
 app.use(express.static('build'));
 app.use(express.json());
 app.use(setHeaders);
@@ -25,37 +35,62 @@ app.get('/api/notes', (request, response) => {
   })
 });
 
-app.get('/api/notes/:id',(request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note);
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+  .then(note => {
+    if (note) {
+      response.json(note);
+    } else {
+      response.status(404).end();
+    }
   })
+  .catch(error => next(error));
 });
 
 app.post('/api/notes', (request, response) => {
   const body = request.body;
-
+  
   if (body.content === undefined) {
     return response.status(400).json({
       error: 'content missing'
     });
   }
-
+  
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
   });
-
+  
   note.save().then(savedNote => {
     response.json(savedNote);
   });
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter(note => note.id !== id);
-  response.status(204).end();
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote);
+    })
+    .catch(error => next(error));
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
 });
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
