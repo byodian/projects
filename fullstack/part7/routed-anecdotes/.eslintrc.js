8bbc1d838ad8a3e5a166c4a8f4767bdd2b30fc1c
byoidan <byj6696@gmail.com>
2021-03-15 15:52:07 +0800

    Adding a anecdotes app

---
+++ fullstack/part7/routed-anecdotes/.eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'node': true,
+    'es6': true
+  },
+  'parserOptions': {
+    'sourceType': 'module',
+    'ecmaVersion': 2018,
+    'ecmaFeatures': {
+      'jsx': true
+    }
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'indent': ['error', 2],
+    'semi': ['error', 'always'],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'object-curly-spacing': ['error', 'always'],
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'react/prop-types': 0,
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'react/no-unescaped-entities': 0
+  },
+  'settings': {
+    'react': {
+      'version': 'detect'
+    }
+  }
+};
--- fullstack/part7/routed-anecdotes/src/index.js
+++ fullstack/part7/routed-anecdotes/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
 import {
-  BrowserRouter as Router 
+  BrowserRouter as Router
 } from 'react-router-dom';
 
 ReactDOM.render(
